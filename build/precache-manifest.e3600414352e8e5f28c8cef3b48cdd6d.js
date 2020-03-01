52a772a4ad9c22f2c3b866807647fe98efe2602d
vladislav1234000 <61660936+vladislav1234000@users.noreply.github.com>
2020-03-01 19:57:36 +0300

    Add files via upload

---
+++ build/precache-manifest.e3600414352e8e5f28c8cef3b48cdd6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1bcde44a172fdfbede01fe444e8dfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed317dbbc0e7851d5100",
+    "url": "/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "ed317dbbc0e7851d5100",
+    "url": "/static/js/2.f600b679.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.f600b679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7468b06e7a7daa4cd0b7",
+    "url": "/static/js/main.d587fb20.chunk.js"
+  },
+  {
+    "revision": "e0d7fe0d40f52d349ba7",
+    "url": "/static/js/runtime-main.bcf2140e.js"
+  }
+]);
